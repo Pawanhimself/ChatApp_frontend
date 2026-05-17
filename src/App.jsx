7515ffb9c2359cf409d7ff0af3af9bcd7145fac9
snainfotech <113756891+Pawanhimself@users.noreply.github.com>
2026-05-17 09:00:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import AuthPage from './pages/Auth'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <AuthPage />
+    </>
+  )
+}
+
+export default App
